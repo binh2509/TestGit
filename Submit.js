45778ec8328530d931bc4d102ba03042cc2955d0
Binh Bon <Binh@Macs-MacBook-Pro-7.local>
2017-05-15 19:45:02 +0700

    Firt commit

---
+++ Submit.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Binh on 12/05/2017.
+ */
+function Submit() {
+ var name = document.getElementById("name").value;
+ var email = document.getElementById("email").value;
+ var phone = document.getElementById("phone").value;
+ var message = document.getElementById("message").value;
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: 'http://localhost:8000/save-contact',
+        data: JSON.stringify({
+            name: name,
+            email: email,
+            message: message,
+            phone: phone
+        }),
+        success: function (response) {
+            if (response.success) {
+                alert(response.message);
+            }
+        }
+    });
+}
